da02fcd6e1b4837533621019e7497e4c704b9ef2
Harsimar <harsimar.rattan@gmail.com>
2024-03-20 12:33:46 -0300

    Initial Commit

---
+++ xyz/xyzApp.swift
@@ -0,0 +1,17 @@
+//
+//  xyzApp.swift
+//  xyz
+//
+//  Created by Harsimar singh Rattan on 2024-03-20.
+//
+
+import SwiftUI
+
+@main
+struct xyzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
